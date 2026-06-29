ff506b9db1f2ede8db88e3acbbbd20e656ba71fe
Prisha Raj <rajprisha1505@gmail.com>
2026-06-30 02:03:01 +0530

    Time: 11 ms (80.53%) | Memory: 61.2 MB (13.74%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -0,0 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty() || nums[stack.peek()] > nums[i]) {
+                stack.push(i);
+            }
+        }
+        
+        int maxWidth = 0;
+        
+        for (int j = n - 1; j >= 0; j--) {
+            while (!stack.isEmpty() && nums[j] >= nums[stack.peek()]) {
+                maxWidth = Math.max(maxWidth, j - stack.pop());
+            }
+        }
+        
+        return maxWidth;
+    }
+}
