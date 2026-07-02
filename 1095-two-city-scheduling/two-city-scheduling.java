b3a281a417aa9bbd9165c5a8b8e21a8124584b47
Prisha Raj <rajprisha1505@gmail.com>
2026-07-03 00:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (79.34%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.java
+++ 1095-two-city-scheduling/two-city-scheduling.java
@@ -1,21 +1,37 @@
-import java.util.Arrays;
-
 class Solution {
     public int twoCitySchedCost(int[][] costs) {
-
-        Arrays.sort(costs, (a, b) -> (a[0] - a[1]) - (b[0] - b[1]));
-
-        int total = 0;
         int n = costs.length / 2;
-
+        quickSelect(costs, 0, costs.length - 1, n - 1);
+        int ans = 0;
         for (int i = 0; i < n; i++) {
-            total += costs[i][0];
+            ans += costs[i][0];
+            ans += costs[i + n][1];
         }
-
-        for (int i = n; i < costs.length; i++) {
-            total += costs[i][1];
-        }
-
-        return total;
+        return ans;
     }
-}
+
+    private void quickSelect(int[][] costs, int le, int r, int n) {
+        int pivotInd = pivot(costs, le, r);
+        if (pivotInd > n) {
+            quickSelect(costs, le, pivotInd - 1, n);
+        } else if (pivotInd < n) {
+            quickSelect(costs, pivotInd + 1, r, n);
+        }
+    }
+
+    private int pivot(int[][] costs, int le, int r) {
+        int pivotInd = le;
+        int pivotDiff = costs[le][0] - costs[le][1];
+        int[] pivotCell = costs[le];
+        int j = le + 1;
+        while (j <= r) {
+            if (costs[j][0] - costs[j][1] <= pivotDiff) {
+                costs[pivotInd] = costs[j];
+                pivotInd++;
+                costs[j] = costs[pivotInd];
+                costs[pivotInd] = pivotCell;
+            }
+            j++;
+        }
+        return pivotInd;
+    }}
