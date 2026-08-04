fa1810f346a8e41cd5f14b0998e1040c91e52720
Prisha Raj <rajprisha1505@gmail.com>
2026-08-04 19:20:03 +0530

    Time: 1 ms (100.00%) | Memory: 46.9 MB (53.61%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.java
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -1,22 +1,26 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> findMissingElements(int[] nums) {
-        int max = Arrays.stream(nums).max().getAsInt();
-        int min = Arrays.stream(nums).min().getAsInt();
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        int max = nums[0];
+        int min = nums[0];
 
-        List<Integer> ls = new ArrayList<>();
-
-        for (int i = min + 1; i < max; i++) {
-            int target = i;
-
-            if (Arrays.stream(nums).anyMatch(x -> x == target)) {
-                continue;
-            } else {
-                ls.add(i);
-            }
+        for(int i : nums){
+            max = Math.max(i, max);
+            min = Math.min(i, min);
         }
 
-        return ls;
+        int[] arr = new int[max + 1];
+        for(int i : nums){
+            arr[i]++;
+        }
+
+        for(int i=min;i<max;i++){
+            if(arr[i] == 0)
+                ans.add(i);
+        }
+        return ans;
+
+
     }
 }
