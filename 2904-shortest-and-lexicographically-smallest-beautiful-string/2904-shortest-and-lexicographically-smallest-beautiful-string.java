ccafdb15089a9a8a0f4bd2d6b0b9db1516789d08
Prisha Raj <rajprisha1505@gmail.com>
2026-08-27 00:32:40 +0530

    Time: 1 ms (100%), Space: 44.3 MB (28.86%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,18 +1,30 @@
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
+        int l = 0, ones = 0;
         String ans = "";
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                String x = s.substring(i, j);
-                long n = x.chars().filter(c -> c == '1').count();
-                if (n == k) {
-                    if (ans.equals("") || x.length() < ans.length() ||
-                        (x.length() == ans.length() && x.compareTo(ans) < 0)) {
-                        ans = x;
-                    }
+
+        for (int r = 0; r < s.length(); r++) {
+            if (s.charAt(r) == '1') ones++;
+
+            while (ones > k) {
+                if (s.charAt(l) == '1') ones--;
+                l++;
+            }
+
+            while (ones == k && s.charAt(l) == '0') {
+                l++;
+            }
+
+            if (ones == k) {
+                String x = s.substring(l, r + 1);
+
+                if (ans.equals("") || x.length() < ans.length() ||
+                    (x.length() == ans.length() && x.compareTo(ans) < 0)) {
+                    ans = x;
                 }
             }
         }
+
         return ans;
     }
 }
